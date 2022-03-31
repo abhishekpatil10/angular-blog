82a3d6b8091ffa845ab20a7cc500f13976f48d5c
abhishekpatil10 <abhishekpatil396$gmail.com>
2022-03-31 11:46:43 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+// import * as $ from 'jquery';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'BLOG-APP';
+  
+  isShowDiv = true;  
+     
+  toggleDisplayDiv() {  
+    this.isShowDiv = !this.isShowDiv;  
+  }    
 }
+
+
+
+
